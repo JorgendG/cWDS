e4402fc23be47eef495c274780f3807ab8eed4ed
Jorgen de Gier <jorgen.de.gier@salomon-it.nl>
2019-12-30 18:23:31 +0100

    answer added

--- cWDS.psm1
+++ cWDS.psm1
@@ -89,7 +89,7 @@ enum Ensure
             elseif( -not (Get-WdsInstallImageGroup -Name $this.GroupName) )
             {
                 Write-Verbose "Creating ImageGroup"
-                New-WdsInstallImageGroup $this.groupname
+                New-WdsInstallImageGroup -Name $this.groupname
                 Write-Verbose "Install image, importing"
                 Import-WdsInstallImage -Path $this.Path -ImageName $this.ImageName -ImageGroup $this.GroupName
             }
@@ -167,4 +167,77 @@ enum Ensure
         }
         return $this
     }
+}
+
+[DscResource()]  class cWDSServerAnswer ##  MyFolder is the name of the resource
+{
+    [DscProperty(Mandatory)]
+    [Ensure]$Ensure
+
+    [DscProperty(Key)]
+    [String]$Answer
+
+    ## What to do if it's  not in the right state. This returns nothing, indicated by [void].
+
+    [void] Set() 
+    {
+        if ($this.Ensure -eq  [Ensure]::Present)
+        {
+            & wdsutil.exe /set-server /AnswerClients:"$($this.Answer)"
+        }
+        elseif ($this.Ensure  -eq [Ensure]::Absent)
+        {
+            & wdsutil /set-server /AnswerClients:none
+        }
+    }
+
+  ## Test to ensure  it's in the right state. This returns a Boolean value, indicated by [bool].
+
+  [bool] Test() 
+    {
+        #                     HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WDSServer\Providers\WDSPXE\Providers\BINLSVC
+        $clientsknown = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\WDSServer\Providers\WDSPXE\Providers\BINLSVC -Name "netbootAnswerOnlyValidClients" -ErrorAction SilentlyContinue
+        $clientsnone = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\WDSServer\Providers\WDSPXE\Providers\BINLSVC -Name "netbootAnswerRequests" -ErrorAction SilentlyContinue
+
+        if( $clientsnone.netbootAnswerRequests -eq "FALSE" )
+        {
+            $answerreg = "none"
+        }
+        elseif( $clientsknown.netbootAnswerOnlyValidClients -eq "TRUE" )
+        {
+            $answerreg = "known"
+        }
+        else
+        {
+            $answerreg = "all"
+        }
+
+        if ($this.Ensure -eq  [Ensure]::Present)
+        {
+            return $answerreg -eq $this.Answer
+        }
+        else
+        {
+            return $answerreg -eq 'none'
+        }
+    }
+
+  ## Get the state.  This returns an instance of the class itself, indicated by [MyFolder]
+
+    [cWDSServerAnswer] Get()  
+    {
+        #
+        $clientsnone = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\WDSServer\Providers\WDSPXE\Providers\BINLSVC -Name "netbootAnswerRequests" -ErrorAction SilentlyContinue
+
+        if( $clientsnone.netbootAnswerRequests -eq "TRUE" )
+        {
+            $this.Ensure = [Ensure]::Present
+        }
+        else 
+        {
+            $this.Ensure = [Ensure]::Absent
+        }
+        return $this
+    }
+   
 }
